f813c8a38a695bfce6edbf7f1fa1fb7b627d70f6
Dillon Tran <dillonctran@gmail.com>
2026-01-03 16:59:51 -0800

    turned index file into binary for faster parsing, added token_meta file

--- src/main/java/com/example/Indexer.java
+++ src/main/java/com/example/Indexer.java
@@ -10,6 +10,13 @@ import java.io.Reader;
 
 import java.util.*;
 
+import java.nio.file.StandardOpenOption;
+import java.nio.channels.FileChannel;
+import java.nio.ByteBuffer;
+import java.nio.file.Path;
+
+import java.io.File;
+
 
 class TokenResult {
     int tf;
@@ -30,6 +37,18 @@ class Posting {
     }
 }
 
+class TokenMeta {
+    int df;
+    long offset;
+    int length;
+
+    TokenMeta(int df, long offset, int length) {
+        this.df = df;
+        this.offset = offset;
+        this.length = length;
+    }
+}
+
 class DocMeta {
     String url;
     int length;
@@ -39,6 +58,7 @@ class DocMeta {
         this.length = length;
     }
 }
+
 
 class PostingTfIdf {
     int docId;
@@ -57,6 +77,7 @@ public class Indexer {
 
     static Map<String, List<Posting>> invertedIndex = new HashMap<>();
     static Map<Integer, DocMeta> docMetadata = new HashMap<>();
+    static Map<String, TokenMeta> tokenMetadata = new HashMap<>();
 
     public static void main(String[] args) {
         indexDirectory();
@@ -67,69 +88,153 @@ public class Indexer {
 
         writeIndex();
         writeDocMetadata();
+        writeTokenMetadata();
+
+        List<Posting> test = getTokenPostings("wic");
+        System.out.println("Size of \"wic\" postings from retrieval " + test.size());
+
+        Map<Integer, DocMeta> doctest = readDocMetadata();
+        DocMeta t = doctest.get(0);
+
+        System.out.println("doc id 0: " + t.url + " " + t.length);
     }
 
-    public static void writeTfIdfMap(Map<String, List<PostingTfIdf>> tfIdfMap) {
-    List<String> sortedTerms = new ArrayList<>(tfIdfMap.keySet());
-    Collections.sort(sortedTerms); // optional: alphabetical order
 
-    try (FileWriter writer = new FileWriter("term_tf_idf.txt")) {
+    public static List<Posting> getTokenPostings(String token) {
+        TokenMeta tm = tokenMetadata.get(token);
+        int df = tm.df;
+        long offset = tm.offset;
+        int length = tm.length; 
 
-        for (String term : sortedTerms) {
-            writer.write(term + " ->");
+        List<Posting> postings = new ArrayList<>(df);
 
-            List<PostingTfIdf> postings = tfIdfMap.get(term);
-            for (PostingTfIdf p : postings) {
-                // write as docId:tfIdf
-                writer.write(" " + p.docId + ":" + String.format("%.6f", p.tfIdf));
+        try (FileChannel channel = FileChannel.open(Path.of("index.bin"), StandardOpenOption.READ)) {
+            ByteBuffer buf = ByteBuffer.allocate(length);
+            channel.read(buf, offset);
+            buf.flip();
+
+            for (int i = 0; i < df; i++) {
+                int docId = buf.getInt();
+                int tf = buf.getInt();
+                int tagMask = buf.getInt();
+
+                postings.add(new Posting(docId, tf, tagMask));
             }
-
-            writer.write("\n");
+        } catch (IOException e){
+            e.printStackTrace();
         }
 
-        System.out.println("TF-IDF map written to " + "term_tf_idf.txt");
-
-    } catch (IOException e) {
-        e.printStackTrace();
+        return postings;
     }
-}
 
     public static void writeIndex() {
         List<String> sortedInvertedTokens = new ArrayList<>(invertedIndex.keySet());
         Collections.sort(sortedInvertedTokens);
-        
-        // You can use this to write to file, etc...
-        try (FileWriter writer = new FileWriter("inverted_index.txt")) {
-            for (String term :sortedInvertedTokens) {
-                writer.write(term + " -> ");
+
+        try (FileChannel channel = FileChannel.open(Path.of("index.bin"), StandardOpenOption.CREATE,
+                                                                          StandardOpenOption.WRITE,
+                                                                          StandardOpenOption.TRUNCATE_EXISTING)) {
+
+            for (String term : sortedInvertedTokens) {
+                String line = term;
 
                 List<Posting> postings = invertedIndex.get(term);
 
+
+                int df = postings.size();
+                long offset = channel.position();
+                int length = postings.size() * Integer.BYTES * 3; // 3 since posting has 3 ints of information
+
+                ByteBuffer buf = ByteBuffer.allocate(length);
+
                 for (Posting p : postings) {
-                    writer.write(" " + p.docId + ":" + p.tf + ":" + p.tagMask);
+                    buf.putInt(p.docId);
+                    buf.putInt(p.tf);
+                    buf.putInt(p.tagMask);
                 }
 
-                writer.write("\n");
+                buf.flip(); // make it ready for channel write
 
-
+                channel.write(buf);
+                tokenMetadata.put(term, new TokenMeta(df, offset, length));
             }
+        } catch (IOException e){
+            e.printStackTrace();
         }
-        catch (IOException e){
+    }
+
+    public static Map<Integer, DocMeta> readDocMetadata() {
+        Map<Integer, DocMeta> docMetadata = new HashMap<>();
+
+        try (Scanner scanner = new Scanner(new File("doc_meta.txt"))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine().trim();
+                if (line.isEmpty()) continue;
+
+                String[] parts = line.split(" ");
+                if (parts.length != 3) continue;
+
+                int docId = Integer.parseInt(parts[0]);
+                String url = parts[1];
+                int length = Integer.parseInt(parts[2]);
+
+                docMetadata.put(docId, new DocMeta(url, length)); 
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
+        return docMetadata;
     }
+
     public static void writeDocMetadata() {
-    try (FileWriter writer = new FileWriter("doc_metadata.txt")) {
-        for (Map.Entry<Integer, DocMeta> entry : docMetadata.entrySet()) {
-            int docId = entry.getKey();
-            DocMeta meta = entry.getValue();
-            writer.write(docId + "|" + meta.url + "|" + meta.length + "\n");
+        try (FileWriter writer = new FileWriter("doc_meta.txt")) {
+            for (Map.Entry<Integer, DocMeta> entry : docMetadata.entrySet()) {
+                int docId = entry.getKey();
+                DocMeta meta = entry.getValue();
+                writer.write(docId + " " + meta.url + " " + meta.length + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-    } catch (IOException e) {
-        e.printStackTrace();
     }
-}
+    
+    public static Map<String, TokenMeta> readTokenMetadata() {
+        Map<String, TokenMeta> tokenMetadata = new HashMap<>();
+
+        try (Scanner scanner = new Scanner(new File("token_meta.txt"))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine().trim();
+                if (line.isEmpty()) continue;
+
+                String[] parts = line.split(" ");
+                if (parts.length != 4) continue;
+
+                String token = parts[0];
+                int df = Integer.parseInt(parts[1]);
+                long offset = Long.parseLong(parts[2]);
+                int length = Integer.parseInt(parts[3]);
+
+                tokenMetadata.put(token, new TokenMeta(df, offset, length)); 
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return tokenMetadata;
+    }
+    
+    public static void writeTokenMetadata() {
+        try (FileWriter writer = new FileWriter("token_meta.txt")) {
+            for (Map.Entry<String, TokenMeta> entry : tokenMetadata.entrySet()) {
+                String token = entry.getKey();
+                TokenMeta meta = entry.getValue();
+                writer.write(token + " " + meta.df + " " + meta.offset + " " + meta.length + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 
     public static void indexDirectory() {
@@ -187,6 +292,14 @@ public class Indexer {
         docMetadata.put(currentDocId, new DocMeta(url, docLength));
     }
 
+
+    // FOR SEARCH
+    //
+    //
+    //
+    //
+    //
+
     public  static Map<String, List<PostingTfIdf>> computeTfIdf(Map<String,List<Posting>> invertedIndex, Map<Integer, DocMeta> docMetadata) {
         Map<String, List<PostingTfIdf>> tfIdfMap = new HashMap<>();
         int totalDocs = docMetadata.size();
@@ -214,5 +327,30 @@ public class Indexer {
     
     return tfIdfMap;
     }
+
+    public static void writeTfIdfMap(Map<String, List<PostingTfIdf>> tfIdfMap) {
+    List<String> sortedTerms = new ArrayList<>(tfIdfMap.keySet());
+    Collections.sort(sortedTerms); // optional: alphabetical order
+
+    try (FileWriter writer = new FileWriter("term_tf_idf.txt")) {
+
+        for (String term : sortedTerms) {
+            writer.write(term + " ->");
+
+            List<PostingTfIdf> postings = tfIdfMap.get(term);
+            for (PostingTfIdf p : postings) {
+                // write as docId:tfIdf
+                writer.write(" " + p.docId + ":" + String.format("%.6f", p.tfIdf));
+            }
+
+            writer.write("\n");
+        }
+
+        System.out.println("TF-IDF map written to " + "term_tf_idf.txt");
+
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+}
 }
 
